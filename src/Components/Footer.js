191b858faf9166a0b3eae1d11709ab0f37e0f781
Gaelprod974 <gaelprod974@gmail.com>
2024-11-25 13:47:11 +0400

    footer1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,37 @@
+import { Link} from 'react-router-dom';
+
 
 function Footer() {
   return (
     <footer class="footer">
         <div class="footer-column">
-            <h4>Colonne 1</h4>
-            <p>Contenu de la première colonne.</p>
+            <h4>John Doe</h4>
+            <p>40 rue Laure Diebold</p>
+            <p>69009 Lyon, France</p>
+            <p>10 20 30 40 50 60</p>
+            <p>john.doe@gmail.com</p>
         </div>
         <div class="footer-column">
-            <h4>Colonne 2</h4>
-            <p>Contenu de la deuxième colonne.</p>
+        <h4>Liens Utiles</h4>
+            <div class="liensUtiles">
+                <Link to="/">Accueil</Link>
+                <Link to="/Services">Services</Link>
+                <Link to="/PorteFolio">Portfolio</Link>
+                <Link to="/Contact">Contact</Link>
+                <Link to="/MentionsLegales">Mention Légales</Link>
+            </div>
+           
         </div>
         <div class="footer-column">
-            <h4>Colonne 3</h4>
-            <p>Contenu de la troisième colonne.</p>
+        <h4>Mes dernières réalisations</h4>
+            <div class="MesDernieresRealisations">
+                <Link to="/PorteFolio">Fresh Food</Link>
+                <Link to="/PorteFolio">Restaurant Akira</Link>
+                <Link to="/PorteFolio">Espace bien-être</Link>
+                <Link to="/PorteFolio">SEO</Link>
+                <Link to="/PorteFolio">Création d'une API</Link>
+                <Link to="/PorteFolio">Maquette d'un site</Link>
+            </div> 
         </div>
     </footer>
       )
