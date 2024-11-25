1c31d423a3a9509f5c9188379684819335a652ed
Gaelprod974 <gaelprod974@gmail.com>
2024-11-25 07:55:08 +0400

    Ajout compenent Footer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
 
 
 function App() {
     return (
         <>
           <header>
-              <NavBar />
+            <NavBar />
           </header>
           <main>
             
           </main>
-          <footer class="footer">
-            <div class="footer-column">
-              <h4>Colonne 1</h4>
-              <p>Contenu de la première colonne.</p>
-            </div>
-            <div class="footer-column">
-              <h4>Colonne 2</h4>
-              <p>Contenu de la deuxième colonne.</p>
-            </div>
-            <div class="footer-column">
-              <h4>Colonne 3</h4>
-              <p>Contenu de la troisième colonne.</p>
-            </div>
-        </footer>
+          <footer>
+            <Footer />
+          </footer>
 
         </>
     );
